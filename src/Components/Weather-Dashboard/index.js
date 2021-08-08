779b47af7dba4ab958eae1de60a820450738fa66
Joel Straley <joelstraley@gmail.com>
2021-08-08 15:56:27 -0400

    Input Form now submits with ENTER key AND button click

--- src/Components/Weather-Dashboard/index.js
+++ src/Components/Weather-Dashboard/index.js
@@ -23,17 +23,27 @@ export default function WeatherDashboard() {
     }
 
     const handleSubmit = e => {
+            e.preventDefault();
             getWeather()
         }
+
+    const handleKeypress = e => {
+        /* if ENTER is hit run handleSubmit */
+        if (e.keyCode === 13) {
+        handleSubmit();
+        }
+    };
 
     const getWeather = () => {
         Axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${search},us&appid=${REACT_APP_APIKEY}`)
         .then((response) =>  {
             
+            /* API has Weather Description in lower case, below capitalizes each word*/
             let string = response.data.weather[0].description
             const capitalizeDescription = str => str.replace(/^(.)|\s+(.)/g, c => c.toUpperCase());
             let desc = capitalizeDescription(string); 
 
+            /* converts temperatures from Kelvin to Fahrenheit */
             let Main = Math.round((response.data.main.temp - 273.15) * 9/5 + 32)
             let Mix = Math.round((response.data.main.temp_min / 273.15) * 9/5 + 32)
             let Max = Math.round((response.data.main.temp_max / 273.15) * 9/5 + 32)
@@ -64,13 +74,12 @@ export default function WeatherDashboard() {
      
 
     /* TODO: 
-    - add Message if no zip code or error
     - CSS styling 
     -fill out README */
 
     return (
-        <div>
-            <h2>{message?message:null}</h2>
+        <div className="container">
+            <h2>{message ? message:null}</h2>
             {Data.Name ? <h1>{Data.Name}</h1> : <></> }
             {Data.Icon ? <img src={Data.Icon} alt="weather icon"/> : <></> }
             {Data.Weather ? <h2>{Data.Weather}</h2> : <></> }
@@ -79,8 +88,11 @@ export default function WeatherDashboard() {
             {Data.MaxTemp ? <h5>{Data.MaxTemp}°</h5> : <></> }
 
                 <hr></hr>
-                <input type="text" onChange={handleChange}></input>
+            <form onClick={handleKeypress}>
+                <label for="zip">Zip Code:</label><br />
+                <input type="text" name="zip" onChange={handleChange}></input>
                 <button onClick={handleSubmit}>Update</button>
+            </form>
         </div>
     )
 }
