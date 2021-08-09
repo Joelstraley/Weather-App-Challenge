45fe5a547c8a1989df99c473faffdff4cb449a50
Joel Straley <joelstraley@gmail.com>
2021-08-09 18:34:14 -0400

    updated CSS margins and changed overall font color to dark gray

--- src/Components/WeatherDisplay/index.js
+++ src/Components/WeatherDisplay/index.js
@@ -1,27 +1,54 @@
 import React from 'react'
-import "./style.css"
+import './style.css'
 
-export default function WeatherDisplay({City, Icon, Weather, MainTemp, MinTemp, MaxTemp}) {
-
+export default function WeatherDisplay({
+  City,
+  Icon,
+  Weather,
+  MainTemp,
+  MinTemp,
+  MaxTemp,
+}) {
   return (
-      <>
-          <section className="header">              
-                <h1 className="cityName">{City}</h1> 
-                {Icon ? <img src={Icon} alt="weather icon" className="icon"/> : <></>}
-            </section>
+    <>
+      <section className='header'>
+        <h1 className='cityName'>{City}</h1>
+        {Icon ? <img src={Icon} alt='weather icon' className='icon' /> : <></>}
+      </section>
 
-            <section>
-                <h4 className="weatherDescription">{Weather}</h4>
-            </section> 
+      <section>
+        <h4 className='weatherDescription'>{Weather}</h4>
+      </section>
 
-            <section>
-                {MainTemp ? <h3 className="mainTemp">{MainTemp}<sup className="mainTemp degree">&deg;</sup></h3> : <></>}
-            </section>
+      <section>
+        {MainTemp ? (
+          <h3 className='mainTemp'>
+            {MainTemp}
+            <sup className='mainTemp degree'>&deg;</sup>
+          </h3>
+        ) : (
+          <></>
+        )}
+      </section>
 
-            <section className="maxMin">
-                {MinTemp ? <h5 className="minTemp">{MinTemp}<sup className="degree">&deg;</sup></h5> : <></>}
-                {MaxTemp ? <h5 className="maxTemp">{MaxTemp}<sup className="degree">&deg;</sup></h5> : <></>}
-            </section>
-      </>
+      <section className='maxMin'>
+        {MinTemp ? (
+          <h5 className='minTemp'>
+            {MinTemp}
+            <sup className='degree'>&deg;</sup>
+          </h5>
+        ) : (
+          <></>
+        )}
+        {MaxTemp ? (
+          <h5 className='maxTemp'>
+            {MaxTemp}
+            <sup className='degree'>&deg;</sup>
+          </h5>
+        ) : (
+          <></>
+        )}
+      </section>
+    </>
   )
 }
